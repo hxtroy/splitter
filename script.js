b1455bfc24a6b8f0e7a16006adb26b923d8cdb24
hxtroy <hxtroy@outlook.com>
2021-11-01 14:38:05 +0000

    RGB boxes

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 "use strict"
 
-alert("hello")
+function split4(){
+    addQuarter(this)
+    addQuarter(this)
+    addQuarter(this)
+    addQuarter(this)
+}
+
+function addQuarter(intoDiv){
+    let q = document.createElement("div")
+    q.classList.add("flexy")
+    q.classList.add("quarter")
+    q.style.backgroundColor=`rgb(${Math.random()*255},${Math.random()*255},${Math.random()*255})`  
+    q.addEventListener("click",split4)
+    intoDiv.appendChild(q)
+}
+
+document.body.addEventListener("click",split4)
